f80f7993fa70b91c1ce0dce1653dd86e3574a4aa
DESKTOP-P0MDIFR\user <shj0000@ex-it.co.kr>
2022-11-18 17:39:39 +0900

    Test Merge

--- src/grid.js
+++ src/grid.js
@@ -4,38 +4,52 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import Example from './example'
 
-export default class MyFirstGrid extends React.Component {
+import { Responsive, WidthProvider } from "react-grid-layout";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+
+export default class MyFirstGrid extends React.Component {  
   render() {
+    const CustomGridItemComponent = React.forwardRef(({style, className, onMouseDown, onMouseUp, onTouchEnd, ...props}, ref) => {
+      return (
+        <div style={{ ...style}} className={className} ref={ref} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onTouchEnd={onTouchEnd}>
+          {/* Some other content */}
+          test
+        </div>
+      );
+    })
+
+  
     // layout is an array of objects, see the demo for more complete usage
     const layout = [
-      { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-      { i: "c", x: 4, y: 0, w: 1, h: 2 }
+      { i: "a", x: 0, y: 0, w: 5, h: 1},
+      { i: "b", x: 1, y: 0, w: 5, h: 1},
+      { i: "c", x: 4, y: 0, w: 5, h: 1}
     ];
+
+
     return (
-      <GridLayout
+      <ResponsiveGridLayout
         className="layout"
         layout={layout}
-        cols={12}
-        rowHeight={30}
-        width={1200}
+        breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
       >
-        <div key="a">a
-          <DndProvider backend={HTML5Backend}>
-            <Example />
-          </DndProvider>
-        </div>
+        <CustomGridItemComponent key="a">
+          aaaass
+
+        </CustomGridItemComponent>
+        {/* <div key="a">a
+        </div> */}
         <div key="b">b
-          <DndProvider backend={HTML5Backend}>
-            <Example />
-          </DndProvider>
         </div>
         <div key="c">c
-          <DndProvider backend={HTML5Backend}>
+          {/* <DndProvider backend={HTML5Backend}>
             <Example />
-          </DndProvider>
+          </DndProvider> */}
         </div>
-      </GridLayout>
+      </ResponsiveGridLayout>
     );
   }
 }
